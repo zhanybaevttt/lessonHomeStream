48fd3951b20d1adb42cd127819b42ea665335a6e
programist <zhanybaevttt@gmail.com>
2022-08-24 18:41:46 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Integer[]array ={1,3,4,6,7,9,13,18,22};
+        List<Integer>list= Arrays.asList(array);
+
+        list.stream().filter(x->x%2==0).map(x->x*x).max(Integer::compare).stream().forEach(System.out::println);
+
+
+
+
     }
 }
